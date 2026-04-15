2c503abbdd3468c91b12d439a25d2e6902c16981
Travis Ross <travisross1691@gmail.com>
2026-04-15 11:05:55 +0100

    fix: :bug: fix sidebar buttons not working on Chrome in landscape mode

--- components/ui/app-sidebar.tsx
+++ components/ui/app-sidebar.tsx
@@ -12,7 +12,7 @@ import SignOutDialog from "../shared/SignOutDialog"
 export function AppSidebar() {
   return (
     <Sidebar>
-      <SidebarHeader className="mt-2">
+      <SidebarHeader className="mt-2 pt-[env(safe-area-inset-top)]">
         <div className="flex items-center gap-2 pl-2">
           <LogoIcon />
           <span className="text-base md:text-sm">Apply Yourself</span>
@@ -24,7 +24,7 @@ export function AppSidebar() {
         </SidebarGroup>
         <SidebarGroup />
       </SidebarContent>
-      <SidebarFooter>
+      <SidebarFooter className="pb-[env(safe-area-inset-bottom)]">
         <SignOutDialog />
       </SidebarFooter>
     </Sidebar>
